8da84735a02b5290ba8e0ae9b3910db85a9178b4
surajverma0619 <suraj@fiblabs.com>
2020-03-18 17:58:02 +0530

    few changes

--- app/src/main/java/com/example/posts/network/NetworkHelper.java
+++ app/src/main/java/com/example/posts/network/NetworkHelper.java
@@ -5,8 +5,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NetworkHelper {
 
-    private final String baseUrl = "https://hn.algolia.com/api/v1/";
-    private Retrofit retrofit = new Retrofit.Builder()
+    private  String baseUrl = "https://hn.algolia.com/api/v1/";
+     Retrofit retrofit = new Retrofit.Builder()
             .baseUrl(baseUrl)
             .addConverterFactory(GsonConverterFactory.create())
             .build();
